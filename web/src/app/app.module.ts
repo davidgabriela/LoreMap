02b4fa1cf8d192a64b530187f3e90c6b6b76952b
GabrielaD <david.gabriela.99@gmail.com>
2022-05-07 23:37:00 +0300

    Added spinner on loading lore collection

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { LayoutModule } from '@angular/cdk/layout';
-import {
-  HttpClientModule, HTTP_INTERCEPTORS
-} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {
-  FormsModule,
-  ReactiveFormsModule
-} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -17,6 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -24,18 +20,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
-import {
-  DialogBodyComponent
-} from './components/dialog-body/dialog-body.component';
-import {
-  LoreCollectionComponent
-} from './components/lore-collection/lore-collection.component';
-import {
-  MainTextPageComponent
-} from './components/main-text-page/main-text-page.component';
-import {
-  NavigationComponent
-} from './components/navigation/navigation.component';
+import { DialogBodyComponent } from './components/dialog-body/dialog-body.component';
+import { LoreCollectionComponent } from './components/lore-collection/lore-collection.component';
+import { MainTextPageComponent } from './components/main-text-page/main-text-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AuthInterceptor } from './services/auth/auth-interceptor';
 
@@ -70,6 +58,7 @@ import { AuthInterceptor } from './services/auth/auth-interceptor';
     MatFormFieldModule,
     MatDialogModule,
     MatInputModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
