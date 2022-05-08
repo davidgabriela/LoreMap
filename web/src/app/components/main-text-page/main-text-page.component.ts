22c122e5a8f67b211f40e32f48114f036435a171
GabrielaD <david.gabriela.99@gmail.com>
2022-05-08 16:05:42 +0300

    Customized text editor

--- web/src/app/components/main-text-page/main-text-page.component.ts
+++ web/src/app/components/main-text-page/main-text-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
+import Quill from 'quill';
 
 @Component({
   selector: 'app-main-text-page',
@@ -10,6 +12,33 @@ export class MainTextPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  blurred = false
+  focused = false
+
+  created(event: Quill) {
+    // tslint:disable-next-line:no-console
+    console.log('editor-created', event)
+  }
+
+  changedEditor(event: EditorChangeContent | EditorChangeSelection) {
+    // tslint:disable-next-line:no-console
+    console.log('editor-change', event)
+  }
+
+  focus($event: any) {
+    // tslint:disable-next-line:no-console
+    console.log('focus', $event)
+    this.focused = true
+    this.blurred = false
+  }
+
+  blur($event: any) {
+    // tslint:disable-next-line:no-console
+    console.log('blur', $event)
+    this.focused = false
+    this.blurred = true
   }
 
 }
