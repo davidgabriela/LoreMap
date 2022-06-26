9825bc5e3fdb8d08e338c50cff1e3cc40013702d
GabrielaD <david.gabriela.99@gmail.com>
2022-06-26 23:50:37 +0300

    Added first ngx-graph

--- web/src/app/components/graph/graph.component.ts
+++ web/src/app/components/graph/graph.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Edge, Node } from '@swimlane/ngx-graph';
 
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
-  styleUrls: ['./graph.component.scss']
+  styleUrls: ['./graph.component.scss'],
 })
 export class GraphComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  public links: Edge[] = [
+    {
+      id: 'a',
+      source: 'first',
+      target: 'second',
+      label: 'is parent of',
+    },
+    {
+      id: 'b',
+      source: 'first',
+      target: 'third',
+      label: 'custom label',
+    },
+    {
+      id: 'c',
+      source: 'first',
+      target: 'fourth',
+      label: 'custom label',
+    },
+  ];
+  public nodes: Node[] = [
+    {
+      id: 'first',
+      label: 'A',
+    },
+    {
+      id: 'second',
+      label: 'B',
+    },
+    {
+      id: 'third',
+      label: 'C',
+    },
+    {
+      id: 'fourth',
+      label: 'D',
+    },
+  ];
 }
